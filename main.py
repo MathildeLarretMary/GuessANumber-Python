6fba597ae2fb0de6d93f38616997bd4cbc8ad2f0
MathildeLarretMary <mathilde.l.art@gmail.com>
2022-11-26 17:40:58 +0100

    FixWhileTrue__init__

--- main.py
+++ main.py
@@ -3,22 +3,21 @@ import random
 class Random:
 
     def __init__(self, num_to_guess: int = 0, num: int = 0):
-        
-        # Play game while true
-        while True :
             self.num = num
             self.num_to_guess = num_to_guess
             self.min = 1
             self.max = 10
 
-            self.num_to_guess = self.get_rand(self.min, self.max)
-            self.num = self.get_proposal(self.min, self.max)
+        # Play game while true
+            while True :
+                self.num_to_guess = self.get_rand(self.min, self.max)
+                self.num = self.get_proposal(self.min, self.max)
 
-            if self.num_to_guess == self.num:
-                return print("You Win ! It was -> ", self.num_to_guess), self.try_again()
-            else:
-                print("You Loose... The Number to Guess was -> ", self.num_to_guess)
-                self.try_again()
+                if self.num_to_guess == self.num:
+                    return print("You Win ! It was -> ", self.num_to_guess), self.try_again()
+                else:
+                    print("You Loose... The Number to Guess was -> ", self.num_to_guess)
+                    self.try_again()
 
     # get arandom number between min/max
     def get_rand(self, min: int, max: int) -> int:
